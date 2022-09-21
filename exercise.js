fa725d95a172e65a30d8079cc0cce8ebf20f7a8f
Flavius <flaviusbejan96@yahoo.com>
2022-09-21 11:46:39 +0300

    Finish

---
+++ exercise.js
@@ -0,0 +1,16 @@
+const room = "Master Suit";
+
+switch (room) {
+  case "Basic Room": {
+    console.log("Price: 50€");
+    break;
+  }
+  case "Junior Suite Room": {
+    console.log("Price: 80€");
+    break;
+  }
+  case "Master Suit": {
+    console.log("Price: 100€");
+    break;
+  }
+}
